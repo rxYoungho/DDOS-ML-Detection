cf2177a4a9c72084fb750af64888b3759b0cc9e7
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-06-16 18:23:46 +0900

    Add files via upload

---
+++ DDoS_Detection_demo.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM #CuDNNLSTM
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import csv
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+print(color.BOLD + 'Hello World !' + color.END)
+
+df = pd.read_csv("attack_test.csv")
+df.info()
+with open("attack_test.csv", newline='') as f:
+    reader = csv.reader(f)
+    origin_X_test = list(reader)
+
+num_feat = ['src_bytes','dst_bytes','wrong_fragment','urgent','hot','num_failed_logins','num_compromised','root_shell','su_attempted','num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds','count','srv_count','serror_rate','srv_serror_rate','rerror_rate','srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count','dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate','dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate','dst_host_srv_rerror_rate']
+#Change features with object or string value into numeric numbers
+ord_feat = ['protocol_type', 'service', 'flag']
+#Nom_feat = column value with 0 or 1
+nom_feat = ['land', 'logged_in', 'is_host_login', 'is_guest_login']
+num_feat = ['src_bytes','dst_bytes','wrong_fragment','urgent','hot','num_failed_logins','num_compromised','root_shell','su_attempted','num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds','count','srv_count','serror_rate','srv_serror_rate','rerror_rate','srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count','dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate','dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate','dst_host_srv_rerror_rate']
+
+#Scale the num, ord, nom datasets
+X_test, y_test = df.drop(columns=['class'], axis = 1, inplace=False), df['class'].values
+# print(X_test[1:2])
+
+ohe = OneHotEncoder(sparse=False)
+oe = OrdinalEncoder()
+scalar = StandardScaler()
+
+ohe.fit(X_test[nom_feat].values)
+oe.fit(X_test[ord_feat].values)
+#reshape Test dataset into 3d array
+
+scalar.fit(X_test[num_feat].values)
+X_test_nom = ohe.transform(X_test[nom_feat].values)
+X_test_ord = oe.transform(X_test[ord_feat].values)
+X_test_num = scalar.transform(X_test[num_feat].values)
+X_test = np.concatenate([X_test_ord, X_test_num, X_test_nom], axis=1)
+
+X_test = X_test.reshape((X_test.shape[0],1,X_test.shape[1]))
+y_test = y_test.reshape((y_test.shape[0],1,1))
+
+model = tf.keras.models.load_model("LSTM_DDoS_Detection.model")
+# CATEGORIES = ["",""]
+for i, each in enumerate(X_test):
+    each = each.reshape((each.shape[0],1,each.shape[1]))
+    prediction = model.predict([each])
+    scale_pred = prediction[0][0][0]
+    if scale_pred >= 0.5:
+        print(origin_X_test[i+2])
+        print(color.RED + "DDoS Packet found." + color.END)
+    elif scale_pred < 0.5:
+        print(origin_X_test[i+2])
+        print(color.GREEN + "This packet is a normal Packet." + color.END)
